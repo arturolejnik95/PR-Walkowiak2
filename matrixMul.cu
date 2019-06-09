afe38decc04a775e3dbe4235f7d38fb5b87d9e1a
arturolejnik95 <arturolejnik95@gmail.com>
2019-06-09 03:26:21 +0200

    Drobne zmiany - wariant 5. do napisania

--- matrixMul.cu
+++ matrixMul.cu
@@ -36,8 +36,89 @@
 // Helper functions and utilities to work with CUDA
 #include <helper_functions.h>
 
+/**
+ * Wariant 3.
+ */
 template <int BLOCK_SIZE> __global__ void
 matrixMul3(float *C, float *A, float *B, int width)
+{
+    // Block index
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+
+    // Thread index
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    // Index of the first sub-matrix of A processed by the block
+    int aBegin = width * BLOCK_SIZE * by;
+
+    // Index of the last sub-matrix of A processed by the block
+    int aEnd   = aBegin + width - 1;
+
+    // Step size used to iterate through the sub-matrices of A
+    int aStep  = BLOCK_SIZE;
+
+    // Index of the first sub-matrix of B processed by the block
+    int bBegin = BLOCK_SIZE * bx;
+
+    // Step size used to iterate through the sub-matrices of B
+    int bStep  = BLOCK_SIZE * width;
+
+    // Csub is used to store the element of the block sub-matrix
+    // that is computed by the thread
+    float Csub = 0;
+
+    // Declaration of the shared memory array As used to
+    // store the sub-matrix of A
+    __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+
+    // Declaration of the shared memory array Bs used to
+    // store the sub-matrix of B
+    __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+    
+    // Loop over all the sub-matrices of A and B
+    // required to compute the block sub-matrix
+    for (int a = aBegin, b = bBegin;
+        a <= aEnd;
+        a += aStep, b += bStep)
+    {
+        // Load the matrices from device memory
+        // to shared memory; each thread loads
+        // one element of each matrix
+        As[ty][tx] = A[a + width * ty + tx];
+        Bs[ty][tx] = B[b + width * ty + tx];
+
+        // Synchronize to make sure the matrices are loaded
+        __syncthreads();
+
+        // Multiply the two matrices together;
+        // each thread computes one element
+        // of the block sub-matrix
+#pragma unroll
+
+        for (int k = 0; k < BLOCK_SIZE; ++k)
+        {
+            Csub += As[ty][k] * Bs[k][tx];
+        }
+
+        // Synchronize to make sure that the preceding
+        // computation is done before loading two new
+        // sub-matrices of A and B in the next iteration
+        __syncthreads();
+    }
+
+    // Write the block sub-matrix to device memory;
+    // each thread writes one element
+    int c = width * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+    C[c + width * ty + tx] = Csub;    
+}
+
+/**
+ * Wariant 5.
+ */
+template <int BLOCK_SIZE> __global__ void
+matrixMul5(float *C, float *A, float *B, int width)
 {
     // Block index
     int bx = blockIdx.x;
